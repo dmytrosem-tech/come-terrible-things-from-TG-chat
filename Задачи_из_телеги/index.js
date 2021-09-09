91fdd34cf71a317ffd206438c478201421845669
Dmytro Semeruk <deemoon83@gmail.com>
2021-09-09 11:01:57 +0300

    lol
    Переделал ответ на поиск символов в объекте))))

--- Задачи_из_телеги/index.js
+++ Задачи_из_телеги/index.js
@@ -1,8 +1,19 @@
 // // Задачка на наличие ключей в объекте----------------------------------->
-// const isEmpty = object => Object.keys(object).length === 0
+// const isEmpty = object => !(Object.getOwnPropertyNames(object).length + Object.getOwnPropertySymbols(object).length)
 
-// let shedule = []
+// let shedule = {}
 // console.log(isEmpty(shedule)) // true
 
-// shedule['8:30'] = 'get up'
+// // shedule['8:30'] = 'get up'
 // console.log(isEmpty(shedule)) // false
+
+// // shedule[Symbol('a')] = 'lol'
+// console.log(isEmpty(shedule)) //  false
+
+// console.log(Object.getOwnPropertySymbols(shedule))
+// console.log(Object.getOwnPropertyNames(shedule))
+// console.log(Object.keys(shedule))
+// console.log(shedule)
+
+// const o = {}
+// console.log(Object.getOwnPropertyNames(o))
