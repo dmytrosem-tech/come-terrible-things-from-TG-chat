43eb5217704896bc2b27d2706c9ff7ece43f2aec
Dmytro Semeruk <deemoon83@gmail.com>
2021-09-09 09:59:02 +0300

    09/09/2021

---
+++ Задачи_из_телеги/index.js
@@ -0,0 +1,8 @@
+// // Задачка на наличие ключей в объекте----------------------------------->
+// const isEmpty = object => Object.keys(object).length === 0
+
+// let shedule = []
+// console.log(isEmpty(shedule)) // true
+
+// shedule['8:30'] = 'get up'
+// console.log(isEmpty(shedule)) // false
